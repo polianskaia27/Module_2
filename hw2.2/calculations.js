091651d88374edeea3a613929235d72e76e26f83
sasha_polianskaia <polianskaia27@gmail.com>
2024-09-08 21:58:28 -0300

    added calculations.js

---
+++ hw2.2/calculations.js
@@ -0,0 +1,24 @@
+function increaseBalance(x, y) {
+  x += y;
+  return x;
+}
+
+function decreaseBalance(x, y) {
+  x -= y;
+  return x;
+}
+
+function divideBalanceByAccounts(x, y) {
+  x /= y;
+  return x;
+}
+
+function getRestAfterDivision(x, y) {
+  x %= y;
+  return x;
+}
+
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2));
+console.log(getRestAfterDivision(7000, 3));
